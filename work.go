e5cb7b8963ecfb79b820a732ecd4912844670dae
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2016-11-27 02:41:26 -0800

    parse

--- work.go
+++ work.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"time"
 
 	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
 	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
 	"github.com/uber-go/zap"
 )
 
+// ** TAG Title
+//    PROP1:value PROP2:value
+//    body
+//    body
+//    [date]
 type OrgEntry struct {
-	Title string
-	Tag   string
-	Body  []string
+	Title      string
+	Tag        string
+	Body       []string
+	Properties map[string]string
+	Date       time.Time
 }
 
 func Process(accountID string, errChan chan error) {
@@ -48,25 +56,26 @@ func Process(accountID string, errChan chan error) {
 
 		content, _ := ioutil.ReadAll(reader)
 		var entry *OrgEntry
+		var tag string
 		var entries []*OrgEntry
 		for _, line := range strings.Split(string(content), "\n") {
 			if strings.HasPrefix(line, "** ") {
-				if entry != nil {
-					entries = append(entries, []*OrgEntry{entry}...)
+				// parse tag
+				p := strings.Split(line, " ")
+				if p[1] == "TODO" || p[1] == "DONE" {
+					tag = p[1]
 				}
-				entry = &OrgEntry{Title: line}
+
+				entry = &OrgEntry{Title: line, Tag: tag}
+			} else if strings.HasPrefix(line, "   [") {
+				entry.Date, _ = time.Parse("   [2000-12-30]", line)
+				entries = append(entries, []*OrgEntry{entry}...)
 			} else {
 				entry.Body = append(entry.Body, line)
 			}
 		}
 
-		// Last element
-		if entry.Title != "" {
-			entries = append(entries, []*OrgEntry{entry}...)
-		}
-
 		for _, e := range entries {
-
 			// TODO: Save tasks to google calendar
 			logger.Info(fmt.Sprintf("%#v", e))
 		}
